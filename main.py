8246460450f2861c365af10289ee1b9ddb677928
stephanefab <kouadiostephanefabienbrou@gmail.com>
2026-08-29 13:27:07 +0000

    accepter et valider le body grace à pydantic

--- main.py
+++ main.py
@@ -1,56 +1,19 @@
-from fastapi import FastAPI, Query, Path, Header
+from fastapi import FastAPI, Header, Path, Query
+from pydantic import BaseModel
 
-## initialisation de l'app
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Bienvenue dans mon API"}
+class OrderCreate(BaseModel):
+    product_id: int
+    quantity: int
 
-@app.get("/about")
-def about():
-    return {"name": "Fabien", "age": 27, "job": "Developer"}
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-@app.get("/path/{path_id}")
-def path_test(path_id):
+@app.post("/orders/{order_id}")
+def create_order(order: OrderCreate, order_id: int = Path(ge=1), priority: int = Query(default=1, ge=1, le=5), x_client_name: str | None = Header(default=None)):
     return {
-        "value": path_id,
-        "type": type(path_id).__name__
-    }
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int = Path(ge=1)):
-    return {
-        "user_id": user_id,
-        "message": "Utilisateur trouvé"
-    }
-
-@app.get("/products/{product_id}")
-def get_product(product_id: int = Path(ge=1)):
-    return {
-        "product_id": product_id
-    }
-
-@app.get("/users")
-def get_users(page: int = Query(default=1, ge=1), limit: int = Query(default=10, ge=1, le=50)):
-    return {
-        "limit": limit,
-        "page": page
-    }
-    
-@app.get("/products")
-def get_products(page: int = Query(default=1, ge=1), limit: int = Query(default=10, ge=1, le=50)):
-    return {
-        "limit": limit,
-        "page": page
-    }
-
-@app.get("/headers")
-def get_headers(x_client_name: str = Header(default="")):
-    return {
-        "client": x_client_name
+        "order_id": order_id,
+        "priority": priority,
+        "client": x_client_name,
+        "product_id": order.product_id,
+        "quantity": order.quantity
     }
